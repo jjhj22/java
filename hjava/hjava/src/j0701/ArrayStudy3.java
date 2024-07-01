72a04265c5d7a88a98b801af2449c8092b382aef
jjhj22 <skarnstktn@naver.com>
2024-07-01 17:34:52 +0900

    dd

---
+++ hjava/hjava/src/j0701/ArrayStudy3.java
@@ -0,0 +1,28 @@
+
+package j0701;
+
+public class ArrayStudy3 {
+
+	public static void main(String[] args) {
+		// 배열에 20개의 랜덤 숫자 저장(랜덤 범위-1~30)
+		// 배열에 저장된 데이터 중에서 5의 배수는 몇개인지 출력하기
+		
+//		int rannum[]=new int[20];
+//		
+//		for(int i=0; i<rannum.length; i++) {
+//			rannum[i]=(int)(Math.random()*30)+1;
+//			
+//			
+//	}
+//		int count=0; //5의 배수 갯수를 저장할 변수
+//		for(int i=0; i<rannum.length; i++) {
+//			if(rannum[i]%5 == 0) {
+//				count++;
+//			}
+//		}
+//		System.out.println("5의 배수는 몇 개?:"+count);
+		
+		//배열 생성방법, 배열에 데이터 저장하는 방법
+		//
+}
+}
