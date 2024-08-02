553cde0fc8cb161a9165a71cb60f90a8679c5be5
jjhj22 <skarnstktn@naver.com>
2024-08-02 17:30:07 +0900

    fire friday!!

---
+++ hjava/jstl/src/main/java/DTO/CommentDTO.java
@@ -0,0 +1,29 @@
+package DTO;
+
+import java.time.LocalDate;
+
+import lombok.Data;
+
+
+//@ToString
+@Data
+
+public class CommentDTO {
+	private int commentId;
+	private int boardId;
+	private String writer;
+	private String comment;
+	private LocalDate writeDate;
+	
+	public static CommentDTO of(int cid, int bid, String writer, String comment, LocalDate date) {
+		CommentDTO dto = new CommentDTO();
+		dto.commentId=cid;
+		dto.boardId=bid;
+		dto.writer=writer;
+		dto.comment=comment;
+		dto.writeDate=date;
+		return dto;
+	}
+
+	
+}
